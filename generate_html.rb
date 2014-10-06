092f55c55e131ced2cc49d66ce5ac27666aa4cdf
Guillaume Claret <dev@clarus.me>
2014-10-06 16:26:25 +0200

    Cosmetic

--- generate_html.rb
+++ generate_html.rb
@@ -7,9 +7,9 @@ include(ERB::Util)
 backup = Backup.new("csv")
 
 # Generate the index
-renderer = ERB.new(File.read("index.html.erb"))
+renderer = ERB.new(File.read("index.html.erb", :encoding => "UTF-8"))
 File.open("index.html", "w") do |file|
-  file << renderer.result()
+  file << renderer.result().gsub(/\n\s*\n/, "\n")
 end
 puts "index.html"
 
@@ -18,10 +18,10 @@ for name, versions in backup.packages do
   folder = File.join("history", name)
   FileUtils.mkdir_p(folder)
   for version in versions do
-    renderer = ERB.new(File.read("history.html.erb"))
+    renderer = ERB.new(File.read("history.html.erb", :encoding => "UTF-8"))
     file_name = File.join(folder, "#{version}.html")
     File.open(file_name, "w") do |file|
-      file << renderer.result()
+      file << renderer.result().gsub(/\n\s*\n/, "\n")
     end
     puts file_name
   end
