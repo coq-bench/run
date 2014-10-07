805e5d06ff158cb30471befb3925c64e5dcc1f30
Guillaume Claret <dev@clarus.me>
2014-10-07 10:11:18 +0200

    Pretty-printing of durations

--- generate_html.rb
+++ generate_html.rb
@@ -19,6 +19,26 @@ File.open("html/index.html", "w") do |file|
 end
 puts "index.html"
 
+class Numeric
+  # Pretty-print a duration in seconds
+  def duration
+    secs  = self.to_int
+    mins  = secs / 60
+    hours = mins / 60
+    days  = hours / 24
+
+    if days > 0
+      "#{days} d and #{hours % 24} h"
+    elsif hours > 0
+      "#{hours} h #{mins % 60} m"
+    elsif mins > 0
+      "#{mins} m #{secs % 60} s"
+    elsif secs >= 0
+      "#{secs} s"
+    end
+  end
+end
+
 # Generate the history
 for name, versions in backup.packages do
   FileUtils.mkdir_p("html/history/#{name}")
