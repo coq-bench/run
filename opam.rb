dd7152bf13ee697441c8fc8ae26eb1e2ffed5552
Guillaume Claret <dev@clarus.me>
2014-10-27 14:34:19 +0100

    More reliable listing of packages

--- opam.rb
+++ opam.rb
@@ -2,25 +2,15 @@
 require_relative 'package'
 
 module Opam
-  # The list of versions for a package name. The package must first be uninstalled.
-  def Opam.versions(name)
-    versions = `opam info --field=available-version,available-versions #{name}`.split(":")[-1]
-    versions.split(",").map {|version| version.strip}
-  end
-
-  # The list of all Coq packages, in a reverse "dependencies" order.
-  # Be cautious, it starts by removing all the installed Coq packages. It does
-  # that so it is simpler to list all the available versions for each package.
-  def Opam.all_packages
-    system("opam remove -y `opam list --all --short \"coq:*\"`")
-    `opam list --all --short --sort "coq:*"`.split(" ").map do |name|
-      name = name.strip
-      Opam.versions(name).map {|version| Package.new(name, version)}
-    end.flatten.reverse
+  # The list of all Coq packages in the given repositories.
+  def Opam.all_packages(repositories)
+    repositories.map do |repository|
+      Dir.glob("../#{repository}/packages/*/*")
+    end.flatten.sort
   end
 
   # Add a repository.
   def Opam.add_repository(repository)
-    system("opam", "repo", "add", "--kind=git", repository.to_s, "../#{repository}")
+    system("opam", "repo", "add", "--kind=git", repository, "../#{repository}")
   end
 end
