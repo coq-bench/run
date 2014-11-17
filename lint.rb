6d66c1a5865e4d84e84689eade258794322bdf19
Guillaume Claret <dev@clarus.me>
2014-11-17 08:26:59 +0100

    Fixes

--- lint.rb
+++ lint.rb
@@ -1,18 +1,17 @@
 def puts_usage
-  puts "Usage: ruby run.rb repo"
-  puts "  stable: the stable repository"
-  puts "  testing: the testing repository"
-  puts "  unstable: the unstable repository"
+  puts "Usage: ruby lint.rb repo folder"
+  puts "  repo: stable, testing or unstable"
+  puts "  folder: the folder of a package"
 end
 
 def lint(repository, folder)
   name, version = File.basename(folder).split(".", 2)
   unless name.match(/\A[a-z:\-]+\z/) then
-    puts "Wrong name '#{name.inspect}', expected only small caps (a-z), dashes or colons (-, :)."
+    puts "Wrong name #{name.inspect}, expected only small caps (a-z), dashes or colons (-, :)."
     exit(1)
   end
-  unless repository != "stable" || version.match("\A[0-9]+\.[0-9]+\.[0-9]+\z") then
-    puts "Wrong stable version name '#{version.inspect}', expected three numbers separated by dots."
+  unless repository != "stable" || version.match(/\A[0-9]+\.[0-9]+\.[0-9]+\z/) then
+    puts "Wrong stable version name #{version.inspect}, expected three numbers separated by dots."
   end
 end
 
