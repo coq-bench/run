34bfea2188ca62b4d55261c8790af83731d92ff5
Guillaume Claret <dev@clarus.me>
2015-01-06 12:57:31 +0100

    ulimit for the memory

--- package.rb
+++ package.rb
@@ -30,12 +30,12 @@ class Package
 
   # Install the dependencies of the package.
   def install_dependencies
-    run(["opam", "install", "-y", "--deps-only", to_s])
+    run(["ulimit -Sv 2000000; opam install -y --deps-only #{to_s}"])
   end
 
   # Install the package.
   def install
-    run(["opam", "install", "-y", "--verbose", to_s])
+    run(["ulimit -Sv 2000000; opam install -y --verbose #{to_s}"])
   end
 
   # Remove the package.
