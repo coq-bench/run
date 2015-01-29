fa78f4a37128a0f4adbd847aeab2d62405591640
Guillaume Claret <dev@clarus.me>
2015-01-29 11:23:39 +0100

    Timeout of 10 minutes of installation of dependencies and packages

--- package.rb
+++ package.rb
@@ -30,12 +30,12 @@ class Package
 
   # Install the dependencies of the package.
   def install_dependencies
-    run(["ulimit -Sv 2000000; opam install -y --deps-only #{to_s}"])
+    run(["ulimit -Sv 2000000; timeout 600 opam install -y --deps-only #{to_s}"])
   end
 
   # Install the package.
   def install
-    run(["ulimit -Sv 2000000; opam install -y --verbose #{to_s}"])
+    run(["ulimit -Sv 2000000; timeout 600 opam install -y --verbose #{to_s}"])
   end
 
   # Remove the package.
