b55671730d9e29a0f188c0a106b9c04bccf30920
Guillaume Claret <dev@clarus.me>
2015-02-23 21:20:18 +0100

    Fixes

--- loop.rb
+++ loop.rb
@@ -1,9 +1,9 @@
 # An infinite loop to run all the benches.
 
-# repositories = ["stable", "unstable"]
-repositories = ["stable"]
+repositories = ["stable", "unstable"]
+# repositories = ["stable"]
 modes = {
-  clean: ["8.5.dev", "dev", "hott"],
+  clean: ["8.4.5", "8.5.dev", "dev", "hott"],
   tree: ["8.5.dev", "dev"] }
 
 while true do
@@ -11,19 +11,20 @@ while true do
     for mode, coqs in modes do
      for coq in coqs do
         # Initialize OPAM.
+        system("rm -Rf ~/.opam*")
         system("opam init -n")
         # Install Coq.
         system("opam repo add coqs https://github.com/coq/repo-coqs.git")
-        system("opam install -y coq.#{coq}")
+        # system("opam install -y coq.#{coq}")
         # Add the repositories.
-        system("git clone https://github.com/coq/repo-stable.git stable")
-        system("git clone https://github.com/coq/repo-unstable.git unstable")
+        system("git clone https://github.com/coq/repo-stable.git ../stable")
+        system("git clone https://github.com/coq/repo-unstable.git ../unstable")
         # Run the bench.
         system("ruby #{mode}.rb #{repository} ../database/#{mode}")
         # Update the HTML.
-        system("cd ../make-html && ruby make_html.rb ../database html")
-        system("cd ../make-html/html && git add .;
-          git commit -m \"Coq #{coq}, OPAM #{opam}, OCaml #{ocaml}, repo #{repository}, mode #{mode}.\";
+        system("cd ../make-html-master && ruby make_html.rb ../database html")
+        system("cd ../make-html-master/html && git pull && git add .;
+          git commit -m \"Coq #{coq}, repo #{repository}, mode #{mode}.\";
           git push")
       end
     end
