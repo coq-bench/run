d1ec1e57838907e9e0324b6f0f5de3e80311e1e3
Guillaume Claret <dev@clarus.me>
2015-04-28 10:29:10 +0200

    8.4.5 added back

--- loop.rb
+++ loop.rb
@@ -3,7 +3,7 @@
 repositories = ["stable", "unstable"]
 # repositories = ["stable"]
 modes = {
-  clean: ["8.4.6", "8.4.dev", "8.5.dev", "dev", "hott"],
+  clean: ["8.4.5", "8.4.6", "8.4.dev", "8.5.dev", "dev", "hott"],
   tree: [] }
 
 while true do
