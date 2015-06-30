bde27fd7a38ee679a0e8f09a28c467636b2674d0
Guillaume Claret <dev@clarus.me>
2015-06-30 11:29:39 +0200

    Fewer benched versions

--- loop.rb
+++ loop.rb
@@ -6,8 +6,8 @@ repositories = ["released", "extra-dev"]
 #   tree: [] }
 
 coqs = {
-  "released" => ["8.3.dev", "8.4.5", "8.4.6~camlp4", "8.4.6", "8.4.dev", "8.5.dev", "dev"],
-  "extra-dev" => ["8.4.dev", "8.5.dev", "dev", "hott"]
+  "released" => ["8.3.dev", "8.4.6~camlp4", "8.4.6", "8.4.dev", "8.5.dev", "dev"],
+  "extra-dev" => ["8.4.dev", "8.5.dev", "dev"]
 }
 
 while true do
