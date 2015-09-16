b060ef82d9030fd0e923b7f0a149b1e8bda471e4
Guillaume Claret <dev@clarus.me>
2015-09-16 16:33:03 +0200

    Nicer timeouts, only the stable packages

--- loop.rb
+++ loop.rb
@@ -1,7 +1,7 @@
 # An infinite loop to run all the benches.
 
-repositories = ["released", "extra-dev"]
-#repositories = ["released"]
+# repositories = ["released", "extra-dev"]
+repositories = ["released"]
 
 # modes = {
 #   clean: ["8.4.5", "8.4.6", "8.4.dev", "8.5.dev", "dev", "hott"],
@@ -19,20 +19,22 @@ while true do
       # Initialize OPAM.
       system("rm -Rf ~/.opam*")
       system("opam init -n")
+      Process.waitall
       # Install Coq.
       system("opam repo add coq-core-dev https://coq.inria.fr/opam/core-dev")
+      Process.waitall
       if system("opam install -y coq.#{coq}") then
         # Add the repositories.
         system("rm -Rf opam-coq-archive && git clone https://github.com/coq/opam-coq-archive.git")
         # Run the bench.
         system("ruby #{mode}.rb #{repository} ../database/#{mode}")
+        Process.waitall
         # Update the HTML.
         system("cd ../make-html-master && ruby make_html.rb ../database html")
         system("cd ../make-html-master/html && git pull && git add .;
           git commit -m \"Coq #{coq}, repo #{repository}, mode #{mode}.\";
           git push")
       end
-      # Clean the zombies.
       Process.waitall
     end
   end
